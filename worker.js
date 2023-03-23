279513e4850e6e5cff8daee073a9ab56cc9f9172
Tobias Buschor <tobias.buschor@shwups.ch>
2023-03-23 17:30:49 +0100

    initial

--- worker.js
+++ worker.js
@@ -1,6 +1,12 @@
+import {dump} from 'https://cdn.jsdelivr.net/gh/nuxodin/dump.js@1.2.1/mod.js';
 addEventListener('message', ({data}) => {
-  postMessage(typeof data.split('.').reduce(
+  const target = data.split('.').reduce(
     ($, k) => $ == null ? $ : $[k],
     globalThis
-  ));
+  );
+  console.log(dump)
+  postMessage({
+    type:typeof target,
+    dumped: dump(target, {depth: 1, inherited: true})
+  });
 });
